192cbfa746dd7dced8216f17a60c56fe3fb4cb8b
carlitos <carmoicast@gmail.com>
2021-07-15 22:43:23 -0500

    Use the db seed with faker gem

--- facturame-api/db/seeds.rb
+++ facturame-api/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  User.create(
+    name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name
+  )
+end
