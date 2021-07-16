abadbd47acbb49e19075a59ae93247370c0c33dc
carlitos <carmoicast@gmail.com>
2021-07-16 11:09:21 -0500

    Add missing fields username, email and password digest

---
+++ facturame-api/db/migrate/20210716143032_add_fields_email_username_password_digest_to_users.rb
@@ -0,0 +1,7 @@
+class AddFieldsEmailUsernamePasswordDigestToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :email, :string
+    add_column :users, :username, :string
+    add_column :users, :password_digest, :string
+  end
+end
